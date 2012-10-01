8fa801c6607dcdde96879339a68fc33ec8bd6a1f
Jakub Stasiak <jakub@stasiak.at>
2012-10-01 21:14:38 +0200

    Redesign activate script

--- activate.py
+++ activate.py
@@ -3,52 +3,27 @@ from __future__ import with_statement
 
 import errno
 
-from os import makedirs, remove
-from os.path import abspath, dirname, exists, expanduser, isdir, isfile, join, split
-from shutil import copy, copytree, rmtree
+from os import makedirs
+from os.path import abspath, dirname, expanduser, join, relpath
+from shutil import copyfile
+
+import os
 
 PROJECT_ROOT = abspath(dirname(__file__))
-LIST_FILE = join(PROJECT_ROOT, 'LIST')
-HOME_DIRECTORY = expanduser('~')
+HOME = expanduser('~')
 FILES_ROOT = join(PROJECT_ROOT, 'files')
 
-def activate_main():
-    copy_all_files()
+def main():
+    for root, directories, files in os.walk(FILES_ROOT):
+        for f in files:
+            activate_file(relpath(join(root, f), FILES_ROOT))
 
-def copy_all_files():
-    files = file_list()
-    copy_files(files)
+def activate_file(rel_file):
+    directory = dirname(rel_file)
+    mkdirp(join(HOME, directory))
+    copyfile(join(FILES_ROOT, rel_file), join(HOME, rel_file))
 
-def file_list():
-    with open(LIST_FILE) as f:
-        return [line.strip() for line in f.readlines() if line and not line.startswith('#')]
-
-def copy_files(files):
-    for file in files:
-        copy_file(file)
-
-def copy_file(file):
-    home_file, local_file = full_paths(file)
-    if exists(local_file):
-        my_copy(local_file, home_file)
-
-def full_paths(file):
-    return join(HOME_DIRECTORY, file), join(FILES_ROOT, file)
-
-def my_copy(src, dst):
-    mkdir_p(dirname(dst))
-    if isdir(dst):
-        rmtree(dst)
-
-    try:
-        copytree(src, dst)
-    except OSError as e:
-        if e.errno == errno.ENOTDIR:
-            copy(src, dst)
-        else:
-            raise
-
-def mkdir_p(path):
+def mkdirp(path):
     try:
         makedirs(path)
     except OSError as e:
@@ -58,6 +33,5 @@ def mkdir_p(path):
             raise
 
 
-
 if __name__ == '__main__':
-    activate_main()
+    main()
