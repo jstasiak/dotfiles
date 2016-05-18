3b67d13d385dec84766613592ffd6d4041afe8e0
Jakub Stasiak <jakub@stasiak.at>
2016-05-18 23:31:08 +0200

    Use Vim configuration in Neovim

--- activate.py
+++ activate.py
@@ -3,8 +3,8 @@ from __future__ import with_statement
 
 import errno
 
-from os import makedirs, remove
-from os.path import abspath, dirname, expanduser, join, relpath
+from os import makedirs, readlink, remove, symlink
+from os.path import abspath, dirname, expanduser, islink, join, relpath
 from shutil import copyfile
 
 import os
@@ -13,10 +13,27 @@ PROJECT_ROOT = abspath(dirname(__file__))
 HOME = expanduser('~')
 FILES_ROOT = join(PROJECT_ROOT, 'files')
 
+
+def copy(src, dst):
+    if islink(src):
+        linkto = readlink(src)
+        if islink(dst):
+            remove(dst)
+        symlink(linkto, dst)
+    else:
+        copyfile(src, dst)
+
+
 def main():
     for root, directories, files in os.walk(FILES_ROOT):
-        for f in files:
+        def activate(f):
             activate_file(relpath(join(root, f), FILES_ROOT))
+        for f in files:
+            activate(f)
+        if not files:
+            for d in directories:
+                if islink(join(root, d)):
+                    activate(d)
     try:
         remove(join(HOME, '.vim', 'bundle', 'vimerl', 'syntax', 'erlang.vim'))
     except OSError as e:
@@ -26,7 +43,7 @@ def main():
 def activate_file(rel_file):
     directory = dirname(rel_file)
     mkdirp(join(HOME, directory))
-    copyfile(join(FILES_ROOT, rel_file), join(HOME, rel_file))
+    copy(join(FILES_ROOT, rel_file), join(HOME, rel_file))
 
 def mkdirp(path):
     try:
